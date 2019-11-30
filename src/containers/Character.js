9a80d27d82d26a1f8bd617d0e127f81467bb10ae
doohinkus <doohinkus@gmail.com>
2019-11-30 15:14:44 -0800

    add game basics

--- src/containers/Character.js
+++ src/containers/Character.js
@@ -4,44 +4,126 @@ import { moveSprite } from '../ducks/characterDuck';
 
 
 function Character({ x_position, y_position, moveSprite, setDirection }){
-  function moveCharacter(direction){
-    const width = 10;
+  // 50px
+  const map = [
+    [0,0,1,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,1,0],
+    [0,0,0,0,0,0,0,0,0,0],
+  ];
+  
+  function moveCharacter(keyCode){
+    let direction = null;
+    const sprite = 10;
+    const height = 300;
+    const width = 500;
+    switch (keyCode){
+      case 38: 
+        direction = "UP"
+        break
+      case 40:
+        direction =  "DOWN"
+        break
+      case 37:
+        direction = "LEFT"
+        break
+      case 39:
+       direction ="RIGHT"
+       break; 
+      }
     switch(direction){
       case ("UP"):
-        moveSprite({ x_position, y_position: y_position - width })
+        if (y_position !== 0){
+          moveSprite({ x_position, y_position: y_position - sprite })
+        }
         break;
       case ("DOWN"):
-        moveSprite({ x_position, y_position: y_position + width })
+        if (y_position !== height - sprite){
+          moveSprite({ x_position, y_position: y_position + sprite })
+        }
         break;
       case ("LEFT"):
-        moveSprite({ x_position: x_position - width, y_position })
+        if (x_position !== 0){
+          moveSprite({ x_position: x_position - sprite, y_position })
+        }
         break;
       case ("RIGHT"):
-        moveSprite({ x_position: x_position + width, y_position })
+        if (x_position !== width - sprite){
+          moveSprite({ x_position: x_position + sprite, y_position })
+        }
         break;
       default: 
         return null
     }
+  
+   
   }
-  function setDirection(keyCode){
-    switch (keyCode){
-      case 38: 
-        return "UP"
-      case 40:
-        return "DOWN"
-      case 37:
-        return "LEFT"
-      case 39:
-       return "RIGHT"
-      default:
-        return null
-    }
-  }
+ 
+  // function setDirection(keyCode){
+  //   switch (keyCode){
+  //     case 38: 
+  //       return "UP"
+  //     case 40:
+  //       return "DOWN"
+  //     case 37:
+  //       return "LEFT"
+  //     case 39:
+  //      return "RIGHT"
+  //     default:
+  //       return null
+  //   }
+  // }
   return (
-    <div tabIndex={0} onKeyDown={e => moveCharacter(setDirection(e.keyCode))}>
+    <div>
       <h2>Character</h2>
+      <p>Click on box then use arrow keys to move sprite.</p>
       <p>x position: {x_position}</p>
       <p>y position: {y_position}</p>
+      <div
+        tabIndex={0} 
+        onKeyDown={e => moveCharacter(e.keyCode)} 
+        style={
+        {
+          position: "relative",
+          margin: "2em auto",
+          backgroundColor: "rgba(255,255,255,.1)",
+          width: "500px",
+          height: "300px",
+          border: "2px solid #222"
+        }
+      }>
+        <div style={
+          {
+            top: y_position,
+            left: x_position,
+            position: "absolute",
+            backgroundColor: "#efefef",
+            width: "10px",
+            height: "10px"
+          }
+        }/>
+        {map.map((x, xIndex) => {
+          return x.map((y, yIndex) => {
+            return (
+              map[xIndex][yIndex] === 1 ? (<div 
+                style={
+                  {
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: "rgba(234,23,1,.3)",
+                    position: "absolute",
+                    top: xIndex * 50,
+                    left: yIndex * 50
+                  }
+                }
+              />) : null
+           
+            )
+          })
+        })}
+      </div>
     </div>
   )
 }
